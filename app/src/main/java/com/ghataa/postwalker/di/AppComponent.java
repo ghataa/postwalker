0841caa7bed77597f5c9644884d6d1312228117c
Gábor Horváth <dev.gaborhorvath@gmail.com>
2019-08-10 09:14:24 +0200

    Init Dagger

---
+++ app/src/main/java/com/ghataa/postwalker/di/AppComponent.java
@@ -0,0 +1,27 @@
+package com.ghataa.postwalker.di;
+
+import android.app.Application;
+
+import com.ghataa.postwalker.App;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Singleton
+@Component(modules = {ApplicationModule.class,
+        AndroidSupportInjectionModule.class})
+public interface AppComponent extends AndroidInjector<App> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        AppComponent.Builder application(Application application);
+
+        AppComponent build();
+    }
+}
